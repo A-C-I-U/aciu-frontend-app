fe53e15912e4af876d2dd68488c3f41709db8b91
Susu-spec <suleimansuwaibat@gmail.com>
2025-07-31 09:18:20 +0100

    feat: add function prop types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,20 @@
 import type React from "react"
 
+export interface AuthCardProps {
+    header?: string,
+    subheader?: React.ReactNode,
+    cardFooter?: boolean,
+    optionalHeader?: boolean,
+    optionalCardHeader?: React.ReactElement,
+    children: React.ReactElement;
+}
+
+export interface FormikPhoneInputProps {
+  name: string;
+  label?: string;
+  defaultCountry?: string;
+}
+
 export interface StepContentProps {
     header: string,
     optionalHeader?: boolean,
@@ -13,4 +28,25 @@ export interface StepContentProps {
 export interface OptionLabelProps {
     value: string,
     label: string
+}
+
+export interface SignUpFormValues {
+    fullName?: string;
+    email?: string;
+    password?: string;
+    confirmPassword?: string;
+    verificationCode?: string;
+    gender?: string;
+    location?: string;
+    branch?: string;
+    village?: string;
+    ageGrade?: string;
+    occupation?: string;
+}
+
+export interface ForgotPasswordValues {
+    email?: string;
+    verificationCode?: string;
+    password?: string;
+    confirmPassword?: string;
 }
