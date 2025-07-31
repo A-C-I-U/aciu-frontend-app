82d3d0e9f6c7ab02595bced6d4253a6228755e33
Susu-spec <suleimansuwaibat@gmail.com>
2025-07-31 09:15:02 +0100

    feat: add card shadow styling

--- src/components/AuthCard.tsx
+++ src/components/AuthCard.tsx
@@ -1,13 +1,7 @@
+import type { AuthCardProps } from "@/utils/types";
 import { Card, CardContent } from "@mui/material";
 
-interface AuthCardProps {
-    header?: string,
-    subheader?: React.ReactNode,
-    cardFooter?: boolean,
-    optionalHeader?: boolean,
-    optionalCardHeader?: React.ReactElement,
-    children: React.ReactElement;
-}
+
 
 
 export default function AuthCard({
@@ -24,6 +18,13 @@ export default function AuthCard({
                 elevation={0}
                 className="min-h-fit w-[31.25rem] rounded-[1.25rem] border border-aciu-card-grey p-8 flex flex-col gap-6 card-shadow"
                 sx={{
+                    boxShadow: `
+                        14px 0px 30px 0px #9191911A,
+                        55px 0px 55px 0px #91919117,
+                        124px 0px 74px 0px #9191910D,
+                        220px 0px 88px 0px #91919103,
+                        344px 0px 96px 0px #91919100
+                    `,
                     borderRadius: '1.25rem',
                     "& .MuiCardContent-root": {
                         padding: 0
