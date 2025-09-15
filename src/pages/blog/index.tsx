5d17ac8c57f1f41564c88bff8ccec1201c202cd8
Susu-spec <suleimansuwaibat@gmail.com>
2025-09-15 15:04:18 +0100

    feat: adds animated tabs to blog and events pages

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -2,6 +2,10 @@ import type { TabItem } from "@/utils/types"
 import BlogPosts from "./components/BlogPosts"
 import { PageTitle } from "@/components/PageTitle";
 import { useState } from "react";
+import { publicationStats } from "@/utils/data";
+import { PublicationCard } from "./components/MyPublications";
+import { motion, AnimatePresence } from "framer-motion";
+import { useUser } from "@/context/UserContext";
 
 const blogsTabs: TabItem[] = [
     { key: "blog-posts", label: "Blog Posts", content: <BlogPosts /> },
@@ -10,22 +14,62 @@ const blogsTabs: TabItem[] = [
 ]
 
 export default function BlogPage() {
-     const [activeTab, setActiveTab] = useState(blogsTabs[0]);
-        const handleTabChange = (tab: TabItem) => {
-            setActiveTab(tab);
-        }
+    const [activeTab, setActiveTab] = useState(blogsTabs[0]);
+    const { user } = useUser();
+
+    const handleTabChange = (tab: TabItem) => {
+        setActiveTab(tab);
+    }
+
+    const visibleTabs = user?.role === "member" ? 
+        blogsTabs.filter(tab => tab.key !== "submissions") :
+        blogsTabs;
     
-        return (
-            <div className="flex flex-col gap-6">
-                <PageTitle
-                    title="ACIU Blog" 
-                    tabs={blogsTabs} 
-                    activeTab={activeTab}
-                    onTabChange={handleTabChange} 
-                />
-                <div className="mx-5 px-4 py-5 bg-white">
+    return (
+        <div className="flex flex-col gap-6">
+            <PageTitle
+                title="ACIU Blog" 
+                tabs={visibleTabs} 
+                activeTab={activeTab}
+                onTabChange={handleTabChange} 
+            />
+
+            <AnimatePresence mode="wait">
+                {activeTab?.key === "my-publications" && (
+                <motion.div
+                    key={activeTab.key}
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 20 }}
+                    transition={{ duration: 0.4, ease: "easeOut" }}
+                    className="mx-5 grid grid-cols-1 lg:grid-cols-3 gap-3"
+                >
+                    {publicationStats.map(({ title, postNumber, rateOfChange }) => (
+                        <PublicationCard 
+                            key={title}
+                            title={title} 
+                            postNumber={postNumber} 
+                            rateOfChange={rateOfChange} 
+                        />
+                    ))}
+                </motion.div>
+                )}
+
+
+            {/* <AnimatePresence mode="wait"> */}
+                <motion.div
+                    key={activeTab?.key + "-content"}
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 20 }}
+                    transition={{ duration: 0.4, ease: "easeOut" }}
+                    className="mx-5 px-4 py-5 bg-white"
+                    >
                     {activeTab?.content}
-                </div>
-            </div>
-        )
+                </motion.div>
+            </AnimatePresence>
+            {/* </AnimatePresence> */}
+        </div>
+
+    )
 }
