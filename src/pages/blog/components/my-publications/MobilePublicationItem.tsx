556c0ad2c30423ed2022f4457e16581094ce247a
Susu-spec <suleimansuwaibat@gmail.com>
2025-09-22 19:42:46 +0100

    feat: adds mobile view for publications table

---
+++ src/pages/blog/components/my-publications/MobilePublicationItem.tsx
@@ -0,0 +1,108 @@
+import { publicationStatusMap } from "@/utils/helpers";
+import type { PublicationDataType } from "@/utils/types";
+import { Eye } from "@solar-icons/react";
+import { CommentOutlined } from "@ant-design/icons";
+import { formatDate } from "@/utils/helpers";
+import { Link } from "react-router-dom";
+
+export default function MobilePublicationItem(publication: PublicationDataType) {
+    const { 
+        title, 
+        creationDate, 
+        lastModified, 
+        postImpressions,
+        status
+    } = publication;
+    const { 
+        label, 
+        labelColor, 
+        dotColor, 
+        bgColor 
+    } = publicationStatusMap[status as PublicationDataType["status"]];
+
+    const { comments, views } = postImpressions;
+
+    return (
+        <div className="w-full py-[1.125rem] px-3 flex flex-col gap-4 items-center rounded-[.625rem] border border-grayscale-200">
+            <div className="flex justify-between items-center w-full">
+                <div className="flex flex-col gap-2 w-full">
+                    <p className="text-xs font-montserrat text-aciu-abriba font-medium">
+                        Title
+                    </p>
+                    <p className="text-sm text-aciu-border-grey font-montserrat overflow-hidden ellipsis">
+                        {title}
+                    </p>
+                </div>
+                 <span 
+                    style={{
+                        backgroundColor: bgColor,
+                        color: labelColor,
+                        height: "2rem"
+                    }} 
+                    className="py-[2px] pr-2 pl-[6px] whitespace-nowrap
+                        flex gap-[6px] items-center rounded-[1rem]
+                        text-xs font-montserrat font-medium max-w-fit"
+                >
+                    <span style={{
+                        backgroundColor: dotColor
+                    }} className="w-[6px] h-[6px] rounded-full"></span>
+                    {label}
+                </span>
+            </div>
+
+            <div className="w-full">
+                <hr className="w-full border-t-[.5px] text-aciu-dark-grey" color="#C9C9C9"/>
+            </div>
+
+            <div className="flex justify-between w-full items-center">
+                <p className="text-xs font-medium font-montserrat text-aciu-abriba">
+                    Creation Date
+                </p>
+                <p className="text-sm font-montserrat text-sm text-aciu-border-grey">
+                    {formatDate(creationDate)}
+                </p>
+            </div>
+
+             <div className="w-full">
+                <hr className="w-full border-t-[.5px] text-aciu-dark-grey" color="#C9C9C9"/>
+            </div>
+
+            <div className="flex justify-between w-full items-center">
+                <p className="text-xs font-medium font-montserrat text-aciu-abriba">
+                    Last Modified
+                </p>
+                <p className="text-sm font-montserrat text-sm text-aciu-border-grey">
+                    {formatDate(lastModified)}
+                </p>
+            </div>
+
+            <div className="w-full">
+                <hr className="w-full border-t-[.5px] text-aciu-dark-grey" color="#C9C9C9"/>
+            </div>
+            <div className="flex justify-between w-full items-center">
+                <p className="text-xs font-medium font-montserrat text-aciu-abriba">
+                    Post Impressions
+                </p>
+                <p className="text-sm font-montserrat text-sm text-aciu-border-grey">
+                    <span className="gap-4 flex items-center">
+                        <span className="gap-2 flex items-center">
+                            <Eye fontVariant="linear" size={16} color="#3E3E3E"/>
+                            {views || '-'}
+                        </span>
+                        <span className="gap-2 flex items-center">
+                            <CommentOutlined size={16} color="#3E3E3E" />
+                            {comments || '-'}
+                        </span>
+                    </span>
+                </p>
+            </div>
+            <Link to={"/blog/post/1"}
+                className="p-2 text-sm font-coolvetica 
+                text-aciu-green-normal rounded-[5px] 
+                border border-aciu-green-normal min-w-[145px] w-full 
+                text-center whitespace-nowrap">
+                View Post
+            </Link>
+        </div>
+    )
+}
--- src/pages/blog/components/my-publications/PublicationsTable.tsx
+++ src/pages/blog/components/my-publications/PublicationsTable.tsx
@@ -6,14 +6,100 @@ import { formatDate } from "@/utils/helpers";
 import { Link } from "react-router-dom";
 import { Eye } from "@solar-icons/react";
 import { CommentOutlined } from "@ant-design/icons";
+import MobilePublicationItem from "./MobilePublicationItem";
+import { useState } from "react";
+import { Pagination } from "@heroui/react";
+import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
+import { useMediaQuery } from "@mui/material";
 
 export default function PublicationsTable() {
+    const isMobile = useMediaQuery('(max-width:768px)')
+    const itemsPerPage = 5;
+    const [page, setPage] = useState(1);
+
+    const start = (page - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const currentItems = mockData.slice(start, end)
+
     return (
-        <DataTable 
-            data={mockData}
-            columns={columns}
-            withSelection
-        />
+        <>
+        {!isMobile ? 
+            <DataTable
+                data={mockData}
+                columns={columns}
+                withSelection
+            /> :
+            currentItems.map((publication, index) => {
+                 const { 
+                    title, 
+                    creationDate, 
+                    lastModified, 
+                    postImpressions,
+                    status
+                } = publication;
+
+                return (
+                    <>
+                        <MobilePublicationItem 
+                            key={index}
+                            title={title} 
+                            creationDate={creationDate} 
+                            lastModified={lastModified} 
+                            postImpressions={postImpressions} 
+                            status={status} 
+                        />
+                    </>
+                )
+            })
+        }
+        {isMobile &&
+            <div className="w-full pt-3 pb-4 px-6 flex justify-between items-center">
+                <button
+                    className="rounded-md py-2 px-[.875rem] 
+                    flex gap-2 justify-center items-center
+                    shadow-[0px_1px_2px_0px_#1018280D] border
+                    border-grayscale-300 "
+                    onClick={() => setPage(page - 1)}
+                    disabled={page === 1}
+                >
+                    <ArrowLeftIcon size={20} color="#00B686" />
+                </button>
+                <Pagination
+                    total={Math.ceil(mockData?.length / itemsPerPage)}
+                    page={page}
+                    onChange={setPage}
+                    classNames={{
+                        cursor: "hidden"
+                    }}
+                    renderItem={(item) => {
+                        const isActive = item.page === page;
+                        return (
+                            <button 
+                                key={item.key}
+                                // onPress is not a prop on button but is required from hero ui
+                                onClick={item.onPress as unknown as React.MouseEventHandler<HTMLButtonElement>}
+                                className={`w-[2.5rem] h-[2.5rem] rounded-md
+                                ${isActive ? "bg-aciu-green-light " : ""}
+                                rounded-md text-aciu-new-green-normal hover:bg-aciu-green-light font-inter`}>
+                                {item.page || item.children}
+                            </button>
+                        )
+                    }}
+                />
+                    <button
+                        className="rounded-md py-2 px-[.875rem] 
+                        flex gap-2 justify-center items-center
+                        shadow-[0px_1px_2px_0px_#1018280D] border
+                        border-grayscale-300 text-green-normal"
+                        onClick={() => setPage(page + 1)}
+                        disabled={page >= Math.ceil(mockData?.length / itemsPerPage)}
+                    >
+                        <ArrowRightIcon size={20} color="#00B686" />
+                    </button>
+                </div>
+            }
+        </>
+       
         
     )
 }
