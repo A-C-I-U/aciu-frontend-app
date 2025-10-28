9be5b2e867592eb7f4b4dbea4d72501e343d94c8
Susu-spec <suleimansuwaibat@gmail.com>
2025-10-28 17:26:46 +0100

    feat: adds `share` and `thank you` prompts

--- src/pages/projects/components/ThankYouPrompt.tsx
+++ src/pages/projects/components/ThankYouPrompt.tsx
@@ -1,6 +1,5 @@
-import AuthCard from "@/components/AuthCard";
+import { MarkIcon } from "@/components/Icons";
 import { Button } from "@mui/material";
-import MarkIcon from "/icons/mark-icon.svg";
 
 export default function ThankYouPrompt({
     title,
@@ -13,30 +12,23 @@ export default function ThankYouPrompt({
  }) {
 
     return (
-        <AuthCard
-            optionalHeader={true}
-            optionalCardHeader={
-                 <div className="flex flex-col items-center gap-6">
-                    <img 
-                        src={MarkIcon}
-                        alt="Green colored background behind a white checkmark"
-                    />
-                    <div className="flex flex-col items-center gap-1.5">
-                        <h1 className="font-coolvetica text-aciu-border-grey font-bold text-[2rem]">
-                            {title}
-                        </h1>
-                        <p className="font-montserrat text-aciu-neutral font-normal">
-                            {description}
-                        </p>
-                    </div>
+        <div className="w-full h-full p-7.5 flex flex-col gap-5.5">
+            <div className="flex flex-col items-start gap-6 w-full h-full">
+                <MarkIcon />
+                <div className="flex flex-col items-start gap-1.5">
+                    <h1 className="font-coolvetica text-aciu-border-grey font-bold text-2xl">
+                        {title}
+                    </h1>
+                    <p className="font-montserrat text-aciu-neutral font-normal">
+                        {description}
+                    </p>
                 </div>
-            }
-        >
+            </div>
             <Button
                 sx={{
                     color: 'white',
                     fontSize: '.75rem',
-                    backgroundColor: '#00CA71',
+                    backgroundColor: '#00B686',
                     borderRadius: '.75rem',
                     padding: '1rem',
                     boxShadow: '0px 1px 2px 0px #0D0D120A',
@@ -47,7 +39,7 @@ export default function ThankYouPrompt({
                         opacity: 0.6,
                     }
                 }}
-                className="flex gap-2 items-center w-full"
+                className="flex gap-2 items-center max-w-fit"
                 onClick={onClose}
             >
                 <span className="font-coolvetica text-base">
@@ -55,6 +47,6 @@ export default function ThankYouPrompt({
                 </span>
             </Button>
 
-        </AuthCard>
+        </div>
     )
 }
