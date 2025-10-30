a7de7b05cc1b72624fca202919a8c64131e63d5d
Susu-spec <suleimansuwaibat@gmail.com>
2025-10-30 20:36:13 +0100

    refactor: remove unnecessary gap when no search/actions are present

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -8,31 +8,31 @@ interface SectionHeaderProps {
   actions?: React.ReactNode[];
 }
 
-const SectionHeader: React.FC<SectionHeaderProps> = ({
+const SectionHeader = ({
   title,
   onSearch,
   showSearch = false,
   actions = [],
-}) => {
+}: SectionHeaderProps) => {
   return (
     <div className="flex justify-between items-center w-full">
-      <div className="flex flex-col lg:gap-4 lg:flex-row lg:justify-between lg:items-center w-full">
+      <div className="flex flex-col gap-6 lg:gap-4 lg:flex-row lg:justify-between lg:items-center w-full">
         <h1 className="whitespace-nowrap text-lg lg:text-xl font-bold text-aciu-border-grey">
           {title}
         </h1>
 
-        <div className="flex gap-4 items-center">
-          {showSearch && (
-            <div className="hidden lg:block">
-              <SearchBar onSearch={onSearch} placeholder={`Search ${title.toLowerCase()}`} />
-            </div>
-          )}
-
-          {actions.length > 0 &&
-            actions.map((action, index) => (
+        {(actions.length > 0) && (
+          <div className="flex gap-4 items-center">
+            {showSearch && (
+              <div className="hidden lg:block">
+                <SearchBar onSearch={onSearch} placeholder={`Search ${title.toLowerCase()}`} />
+              </div>
+            )}
+            {actions.map((action, index) => (
               <React.Fragment key={index}>{action}</React.Fragment>
             ))}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
