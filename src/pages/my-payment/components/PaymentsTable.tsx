1600e19c92a3ba766c4f04ee6ae4d958dc8472b1
Susu-spec <suleimansuwaibat@gmail.com>
2025-11-11 11:39:18 +0100

    feat: adds payments page; table and data

---
+++ src/pages/my-payment/components/PaymentsTable.tsx
@@ -0,0 +1,54 @@
+import { generateMockPayment } from "@/utils/helpers";
+import { useMediaQuery } from "@mui/material";
+import { useState } from "react";
+import { columns } from "./dues-breakdown/columns";
+import MobilePaymentItem from "./MobilePaymentItem";
+import type { PaymentDataType } from "@/utils/types";
+import DataTable from "@/components/DataTable";
+import { getCoreRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table";
+import { PaginationControls } from "@/pages/blog/components/shared/PaginationControls";
+
+export default function PaymentsTable() {
+    const isMedium = useMediaQuery('(max-width:992px)');
+    const itemsPerPage = 5;
+    const [page, setPage] = useState(1);
+
+    const start = (page - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const currentItems = mockData.slice(start, end);
+
+    const table = useReactTable<PaymentDataType>({
+        data: mockData,
+        columns,
+        pageCount: Math.ceil(mockData.length / 10),
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel()
+    })
+
+    return (
+        <>
+            {!isMedium ?
+                <DataTable 
+                    table={table}
+                />
+                :
+                currentItems.map((payment: PaymentDataType) => (
+                    <MobilePaymentItem
+                        key={payment.id}
+                        payment={payment}
+                    />
+                ))}
+            {isMedium &&
+                <PaginationControls
+                    total={mockData.length}
+                    page={page}
+                    onPageChange={setPage}
+                    itemsPerPage={itemsPerPage}
+                />
+            }
+        </>
+    )
+}
+
+
+const mockData: PaymentDataType[] = generateMockPayment(20)
