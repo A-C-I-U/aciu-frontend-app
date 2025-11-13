0489d48e245df456fac3949d5b8d62a8bfa1b9c8
Susu-spec <suleimansuwaibat@gmail.com>
2025-11-13 13:25:39 +0100

    fix: adds `mode=wait` and exit animation to main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -20,25 +20,25 @@ export default function MainLayout() {
             >
                 <Sidebar />
             </aside>
-            <ScrollToTopOnRouteChange>
-                <div className="w-full flex-1 ml-0 md:ml-[14.8rem] ">
-                    <Header />
-                    <main className="bg-aciu-body">
-                        <div className="max-w-[90rem] mx-auto">
-                            <AnimatePresence>
-                                <motion.div
-                                    key={location.pathname}
-                                    initial={{ opacity: 0 }}
-                                    animate={{ opacity: 1 }}
-                                    transition={{ duration: 1, ease: "easeInOut" }}
-                                >
-                                    <Outlet />
-                                </motion.div>
-                            </AnimatePresence>
-                        </div>
-                    </main>
-                </div>
-            </ScrollToTopOnRouteChange>
+            <ScrollToTopOnRouteChange />
+            <div className="w-full flex-1 ml-0 md:ml-[14.8rem] ">
+                <Header />
+                <main className="bg-aciu-body">
+                    <div className="max-w-[90rem] mx-auto">
+                        <AnimatePresence mode="wait">
+                            <motion.div
+                                key={location.pathname}
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 1, ease: "easeInOut" }}
+                            >
+                                <Outlet />
+                            </motion.div>
+                        </AnimatePresence>
+                    </div>
+                </main>
+            </div>
         </div>
     )
 }
