4c2d6d0e4e614916af365352dec3f79c9fdf7ba5
Susu-spec <suleimansuwaibat@gmail.com>
2025-11-17 16:24:50 +0100

    refactor: adds `section-header` component and reusable classes

--- src/pages/resources/aciu-identity/index.tsx
+++ src/pages/resources/aciu-identity/index.tsx
@@ -10,14 +10,13 @@ export default function IdentityPage() {
             
     const handleSearch = (q: string) => {
         setQuery(q);
-        console.log(query);
     }
 
     return (
         <div className="flex flex-col gap-4 lg:gap-8">
             <div className="flex justify-between items-center w-full">
                 <div className="flex flex-col gap-4 lg:flex-row lg:gap-0 lg:justify-between lg:items-center w-full">
-                    <h1 className="font-coolvetica text-lg lg:text-xl font-bold text-aciu-border-grey">
+                    <h1 className="text-lg lg:text-xl font-bold text-aciu-border-grey">
                         Our Voice, Our Symbols, Our Pride
                     </h1>
                     
@@ -29,10 +28,7 @@ export default function IdentityPage() {
                             />
                         </div>
                         <button 
-                            className="flex gap-2.5 items-center p-2.5 
-                            text-sm text-grayscale-100 rounded-md 
-                            font-montserrat font-medium min-h-[50px] 
-                            border border-aciu-card-grey"
+                            className="section-action-button"
                         >
                             Filter
                             <Sort variant="Outline" color="#A4ACB9" size={20} />
@@ -42,8 +38,7 @@ export default function IdentityPage() {
             </div>
             {dummyResources ?
                 <div 
-                    className="grid grid-cols-2 gap-x-2 gap-y-[1.125rem]
-                    md:grid-cols-3 lg:grid-cols-4 md:gap-x-6 md:gap-y-8"
+                    className="resource-grid"
                 >
                     {dummyResources.map((resource, index) => (
                         <FileView key={index} {...resource} />
