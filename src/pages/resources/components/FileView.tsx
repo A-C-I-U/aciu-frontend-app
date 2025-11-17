3888eafb1eccd46ab46da22edf9b15fe9c1f3d34
Susu-spec <suleimansuwaibat@gmail.com>
2025-11-17 16:33:46 +0100

    refactor: replaces `MUI` box components with divs

--- src/pages/resources/components/FileView.tsx
+++ src/pages/resources/components/FileView.tsx
@@ -1,13 +1,11 @@
 import type { FileViewProps } from "@/utils/types";
-import { Box } from "@mui/material";
 import { useState } from "react";
-import FileViewDrawer from "./FileViewDrawer";
 import { formatSize, getExtension } from "@/utils/helpers";
+import ViewResource from "./ViewResource";
 
 export default function FileView({
     file,
     name,
-    description
 }: FileViewProps) {
     const [fileOpen, setFileOpen] = useState(false);
 
@@ -24,67 +22,50 @@ export default function FileView({
 
     return (
         <>
-            <Box
+            <button
                 onClick={handleOpen}
-                borderRadius="1.25rem"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                bgcolor="#F9FBFC"
-                sx={{
-                    py: {
-                        xs: "1.75rem",
-                        md: "3.125rem"
-                    },
-                    px: {
-                        xs: "1.25rem",
-                        md: "2.25rem"
-                    },
-                    cursor: "pointer",
-                        transition: "background-color 0.3s ease, color 0.3s ease",
-                        "&:hover": {
-                            bgcolor: "#00B686",
-                        "& p": {
-                            color: "#fff",
-                        },
-                        "& p.size": {
-                            color: "#C9C9C9"
-                        },
-                        "& p.extension": {
-                            color: "#737373"
-                        }
-                    },
-                }}
-                
-            >
-                <Box
-                    display="flex"
-                    flexDirection="column"
-                    gap={4}
-                    alignItems="center"
-                    justifyContent="center"
+                className="cursor-pointer
+                    rounded-[1.25rem]
+                    flex items-center justify-center
+                    bg-card-200
+                    transition-colors duration-300
+                    px-5 py-7
+                    md:px-9 md:py-12
+                    hover:bg-aciu-green-normal
+                    group"
                 >
-                    <div className="flex items-center justify-center 
-                        border rounded-md w-[5.5rem] h-[5.5rem]
-                        border-aciu-dashboard-background bg-white">
-                        <p className="extension font-montserrat text-sm text-aciu-abriba uppercase">
+                <div className="flex flex-col items-center justify-center gap-4">
+                    <div className="
+                        flex items-center justify-center
+                        border rounded-md
+                        w-22 h-22
+                        border-aciu-dashboard-background
+                        bg-white
+                    ">
+                        <p className="extension text-sm 
+                            uppercase text-aciu-abriba
+                            group-hover:text-aciu-abriba"
+                        >
                             {extension}
                         </p>
                     </div>
                     <div className="flex flex-col gap-2 items-center">
-                        <p className="font-montserrat text-sm font-semibold text-aciu-darker-grey">
+
+                        <p className="text-sm font-semibold text-aciu-darker-grey
+                            group-hover:text-white">
                             {name}
                         </p>
-                        <p className="size font-montserrat text-xs font-medium text-aciu-border-grey">
+
+                        <p className="size text-xs font-medium text-aciu-border-grey
+                            group-hover:text-aciu-dark-grey">
                             {size}
                         </p>
+
                     </div>
-                </Box>
-            </Box>
-            <FileViewDrawer 
-                file={file}
-                name={name}
-                description={description}
+
+                </div>
+            </button>
+            <ViewResource
                 open={fileOpen}
                 onClose={handleClose}
             />
