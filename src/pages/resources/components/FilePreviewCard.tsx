45d41ce9d84e01b111a09edf0915d9803bc4f276
Susu-spec <suleimansuwaibat@gmail.com>
2025-11-17 16:30:58 +0100

    feat: removes unnecessary helpers and styles

--- src/pages/resources/components/FilePreviewCard.tsx
+++ src/pages/resources/components/FilePreviewCard.tsx
@@ -1,4 +1,4 @@
-import { formatSize, getExtension } from "@/utils/helpers";
+import { formatSize, getExtension, getExtensionFromUrl } from "@/utils/helpers";
 
 interface FilePreviewCardProps {
     file: File | string | null | undefined;
@@ -26,26 +26,21 @@ export const FilePreviewCard = ({
 
     return (
         <div className={`mt-16 py-12.5 px-9 ${width} ${height} rounded-xl bg-card-200 flex flex-col items-center justify-center gap-4 ${className}`}>
-            <div className="flex items-center justify-center w-22 h-22 rounded-lg bg-aciu-dashboard-background">
-                <p className="font-montserrat text-sm">
+            <div className="flex items-center justify-center w-22 h-22 rounded-lg border border-aciu-dashboard-background bg-white">
+                <p className="text-sm capitalize">
                     {extension}
                 </p>
             </div>
             {name !== '' &&
-                <p className="font-montserrat text-sm text-center">
+                <p className="text-sm text-center">
                     {name}
                 </p>
             }
             {size !== '' &&
-                <p className="font-montserrat font-medium text-xs text-aciu-border-grey">
+                <p className="font-medium text-xs text-aciu-border-grey">
                     {size}
                 </p>
             }
         </div>
     );
-};
-
-const getExtensionFromUrl = (url: string): string => {
-  const match = url.match(/\.([^./?]+)(?:[?#]|$)/);
-  return match ? match[1] : '';
 };
