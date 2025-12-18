10e90f5488624520fdbebac1fa4aa7fe52552cf8
Susu-spec <suleimansuwaibat@gmail.com>
2025-12-18 16:32:15 +0100

    feat: adds coming soon text to empty pages

---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,16 @@
+export default function ComingSoon() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-aciu-body text-center px-6">
+            <h1 className="font-clash-display text-5xl text-aciu-border-grey mb-4">
+                Coming Soon...
+            </h1>
+
+            <p className="text-lg text-copy-400 mb-8 max-w-md">
+                This section is under construction. Soon you'll be able to view the details of this page. Stay tuned!
+            </p>
+            <p className="mt-12 text-sm text-aciu-abriba font-coolvetica">
+                © 2025 ACIU
+            </p>
+        </div>
+    )
+}
--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,5 +1,7 @@
+import ComingSoon from "@/components/ComingSoon";
+
 export default function Transactions() {
     return (
-        <></>
+        <ComingSoon />
     )
 }
