80839a4dfa9eed39d390e570bf427df572a30206
Susu-spec <suleimansuwaibat@gmail.com>
2025-12-21 12:58:07 +0100

    feat: adds hooks for `all-events`, `upcoming-events`, and `past-events`

--- src/services/hooks/events.ts
+++ src/services/hooks/events.ts
@@ -1,3 +1,4 @@
+import { useUser } from "@/context/UserContext";
 import apiClient from "..";
 import type { EventDetailsResponse, EventsResponse } from "../types/events";
 import { useQuery } from "@tanstack/react-query";
@@ -15,6 +16,50 @@ export const useEvents = () => {
     gcTime: 10 * 60 * 1000,
   });
 };
+
+const fetchAllEvents = async (): Promise<EventsResponse> => {
+  const response = await apiClient.get<EventsResponse>("/events/public")
+  return response.data
+};
+
+export const useAllEvents = () => {
+  const { user } = useUser();
+  return useQuery({
+    queryKey: ["events", "all-events"],
+    queryFn: fetchAllEvents,
+    enabled: user?.role === "national_admin",
+    staleTime: 5 * 60 * 1000,
+    gcTime: 10 * 60 * 1000,
+  })
+}
+
+const fetchUpcomingEvents = async (): Promise<EventsResponse> => {
+  const response = await apiClient.get<EventsResponse>("/events/upcoming")
+  return response.data
+};
+
+export const useUpcomingEvents = () => {
+  return useQuery({
+    queryKey: ["events", "upcoming-events"],
+    queryFn: fetchUpcomingEvents,
+    staleTime: 5 * 60 * 1000,
+    gcTime: 10 * 60 * 1000,
+  })
+}
+
+const fetchPastEvents = async (): Promise<EventsResponse> => {
+  const response = await apiClient.get<EventsResponse>("/events/past")
+  return response.data
+};
+
+export const usePastEvents = () => {
+  return useQuery({
+    queryKey: ["events", "past-events"],
+    queryFn: fetchPastEvents,
+    staleTime: 5 * 60 * 1000,
+    gcTime: 10 * 60 * 1000,
+  })
+}
 
 const fetchEventDetails = async (
   eventId: string
