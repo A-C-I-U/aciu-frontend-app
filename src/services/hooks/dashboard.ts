03ef89ca4b134493c53b0e17c9460348e2774490
Susu-spec <suleimansuwaibat@gmail.com>
2025-12-22 17:08:31 +0100

    feat: adds user role check on endpoints to execute

--- src/services/hooks/dashboard.ts
+++ src/services/hooks/dashboard.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import apiClient from '..';
 import type { DashboardResponse, FinancesResponse, NationalDashboardStats } from '../types/dashboad';
-
+import { useUser } from '@/context/UserContext';
 
 export const useDashboardOverview = () => {
   return useQuery<DashboardResponse, Error>({
@@ -31,8 +31,10 @@ export const useFinances = () => {
 };
 
 export const useNationalDashboardStats = () => {
+  const { user } = useUser();
   return useQuery<NationalDashboardStats, Error>({
     queryKey: ['dashboard', 'nationalAdmin'],
+    enabled: user?.role === "national_admin",
     queryFn: async (): Promise<NationalDashboardStats> => {
       const response = await apiClient.get<NationalDashboardStats>('/dashboard');
       return response.data;
@@ -43,8 +45,10 @@ export const useNationalDashboardStats = () => {
 }
 
 export const useNationalDashboardWithdrawals = () => {
+  const { user } = useUser();
   return useQuery<any, Error>({
     queryKey: ['dashboard', 'nationalAdmin', 'withrawalRequests'],
+    enabled: user?.role === "national_admin",
     queryFn: async (): Promise<any> => {
       const response = await apiClient.get<any>('/dashboard/withdrawals');
       return response.data;
@@ -57,6 +61,7 @@ export const useNationalDashboardWithdrawals = () => {
 export const useWithdrawalDetail = (id: string) => {
   return useQuery<any, Error>({
     queryKey: ['withdrawal', 'detail', id],
+    enabled: !!id,
     queryFn: async (): Promise<any> => {
       const response = await apiClient.get<any>(`/withdrawal/${id}`);
       return response.data;
