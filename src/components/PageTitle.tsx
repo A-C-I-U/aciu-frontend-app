53506696983aed8f9986eb71a52f14e6294bf592
Susu-spec <suleimansuwaibat@gmail.com>
2026-01-07 14:41:01 +0100

    chore: adds id for use in `scrollToPosition`

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -11,7 +11,7 @@ export const PageTitle = ({ title, tabs = [], activeTab, onTabChange }: PageTitl
         <h1 className="font-coolvetica text-xl text-aciu-border-grey">
             {title}
         </h1>
-        <nav role="tablist" className="flex gap-4 md:gap-8 items-center">
+        <nav id="nav-tabs" role="tablist" className="flex gap-4 md:gap-8 items-center">
             {tabs.map((tab) => (
                 <button 
                     key={tab?.key}
