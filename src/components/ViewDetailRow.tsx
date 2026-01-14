69acaa34107a73e5a67dda2c34a2d2f4a9eb7cdf
Susu-spec <suleimansuwaibat@gmail.com>
2026-01-14 09:58:00 +0100

    style: modify styling

--- src/components/ViewDetailRow.tsx
+++ src/components/ViewDetailRow.tsx
@@ -2,8 +2,8 @@
 export const ViewDetailRow = ({ label, content }: { label: string, content: React.ReactNode }) => {
     return (
         <tr>
-            <td className="payment-table-column title whitespace-nowrap">{label}</td>
-            <td className="payment-table-column desc capitalize whitespace-nowrap max-w-92 truncate">{content}</td>
+            <td className="payment-table-column title whitespace-nowrap align-text-top">{label}</td>
+            <td className="payment-table-column desc capitalize align-text-top max-w-92 break-words">{content}</td>
         </tr>
     )
 }
