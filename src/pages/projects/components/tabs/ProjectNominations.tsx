3aa8b9a8899ec4f02d3f2e31426fa1ce9bffa6cc
Susu-spec <suleimansuwaibat@gmail.com>
2026-01-15 16:30:57 +0100

    feat: adds table skeleton on loading

--- src/pages/projects/components/tabs/ProjectNominations.tsx
+++ src/pages/projects/components/tabs/ProjectNominations.tsx
@@ -5,50 +5,53 @@ import { useMediaQuery } from "@mui/material";
 import { useState } from "react";
 import { EmptyPage } from "@/components/EmptyPage";
 import ErrorState from "@/components/ErrorState";
-import { columns } from "./ProjectNomColumns";
-import ProjectNominationsTable from "./ProjectNominationsTable";
+import { columns } from "../table/ProjectNomColumns";
+import ProjectNominationsTable from "../table/ProjectNomTable";
+import TableSkeleton from "@/components/TableSkeleton";
+import MobileItemSkeleton from "@/components/MobileItemSkeleton";
 
 export default function ProjectNominations() {
     const { data: nominatedProjects, isLoading, error } = useNominatedProjects();
     const isMedium = useMediaQuery("(max-width: 1250px)");
     const [_query, setQuery] = useState("");
-    const [showCreate, setShowCreate] = useState(false);
 
     const handleSearch = (q: string) => {
         setQuery(q);
     }
 
     return (
-        <div className="flex flex-col gap-4 lg:gap-8">
-            <div className={`flex ${isMedium ? "items-start" : "items-center"} md:gap-4`}>
-                <SectionHeader
-                    title="Project Nomination"
-                    onSearch={handleSearch}
-                    showSearch={isMedium ? false : true}
-                    actions={sectionActions}
-                />
-                <button 
-                    className="py-3 px-1 text-sm md:text-base md:py-4 md:px-2 gap-2 text-white font-coolvetica bg-aciu-green-normal whitespace-nowrap w-fit rounded-xl"
-                    onClick={() => setShowCreate(true)}
-                >
-                    Create Project
-                </button>
+        <>
+            <div className="flex flex-col gap-4 lg:gap-8">
+                <div className={`flex ${isMedium ? "items-start" : "items-center"} md:gap-4`}>
+                    <SectionHeader
+                        title="Project Nomination"
+                        onSearch={handleSearch}
+                        showSearch={isMedium ? false : true}
+                        actions={sectionActions}
+                    />
+                    
+                </div>
+                {(isLoading) && (!isMedium ? 
+                    (<TableSkeleton />):
+                    (<div className="grid gap-4 md:grid-cols-2">
+                        {[1, 2, 3, 4].map((index) => (
+                            <MobileItemSkeleton key={index}/>
+                        ))}
+                    </div>) 
+                )}
+                {(error) && (
+                    <ErrorState label="project nominations"/>
+                )}
+                {(nominatedProjects && nominatedProjects?.length === 0) && (
+                    <EmptyPage label="No Project Nominations" />
+                )}
+                {(nominatedProjects && nominatedProjects.length > 0) && (
+                    <ProjectNominationsTable
+                        data={nominatedProjects}
+                        columns={columns}
+                    />
+                )}
             </div>
-            {(isLoading) && (
-                <></>
-            )}
-            {(error) && (
-                <ErrorState label="project nominations"/>
-            )}
-            {(nominatedProjects && nominatedProjects?.length === 0) && (
-                <EmptyPage label="No Project Nominations" />
-            )}
-           {(nominatedProjects && nominatedProjects.length > 0) && (
-                <ProjectNominationsTable
-                    data={nominatedProjects}
-                    columns={columns}
-                />
-           )}
-        </div>
+        </>
     )
 }
