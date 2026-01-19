d1b0c8986c8cd3e129d3b7cd5cf7785e8375c891
Susu-spec <suleimansuwaibat@gmail.com>
2026-01-19 19:38:27 +0100

    feat: adds due payments base page

--- src/pages/transactions/components/tabs/DuesPayment.tsx
+++ src/pages/transactions/components/tabs/DuesPayment.tsx
@@ -1,5 +1,61 @@
+import { TransactionsAreaChart } from "@/components/TransactionsAreaChart";
+import { useDuesPayments, useDuesPaymentsVisuals } from "@/services/hooks/transactions"
+import { Divider } from "@mui/material";
+import { ArrowDown2 } from "iconsax-react";
+import DuePaymentsTable from "../tables/DuePaymentsTable";
+
 export default function DuesPayment() {
+    const { data: duesPaymentVisual } = useDuesPaymentsVisuals(2026);
+    const { data: duesPayments } = useDuesPayments();
+
     return (
-        <></>
+        <div className="flex flex-col gap-6 w-full">
+            <div className="bg-white rounded-lg flex flex-col gap-6 min-w-0">
+                <div className="flex flex-col pt-5.5 lg:flex-row gap-6.75 lg:gap-7 justify-between lg:items-center px-4 lg:px-8">
+                    <h3 className="text-xl leading-[120%] text-aciu-border-grey font-bold">
+                        Dues Payment
+                    </h3>
+                    <div className="hidden lg:flex gap-2 items-center">
+                        <button className="section-action-button admin">
+                            2022
+                            <ArrowDown2 color="#3E3E3E" size={14} />
+                        </button>
+                        <button className="section-action-button admin">
+                            Monthly
+                            <ArrowDown2 color="#3E3E3E" size={14} />
+                        </button>
+                    </div>
+                </div>
+                <Divider sx={{ borderColor: "#EEECF6" }}/>
+                <div className="flex lg:hidden gap-2 items-center px-4 lg:px-8">
+                    <button className="section-action-button admin">
+                        2022
+                        <ArrowDown2 color="#3E3E3E" size={14} />
+                    </button>
+                    <button className="section-action-button admin">
+                        Monthly
+                        <ArrowDown2 color="#3E3E3E" size={14} />
+                    </button>
+                </div>
+                
+                <div className="px-4 lg:px-8 max-w-full"> 
+                    <div className="overflow-x-auto"> 
+                        <div className="min-w-125 md:min-w-0 mb-4"> 
+                            <TransactionsAreaChart
+                                data={duesPaymentVisual?.monthlyPayments ?? []}
+                                dataKey="totalPayment"
+                            /> 
+                        </div> 
+                    </div> 
+                </div>
+            </div>
+
+            <div className="bg-white py-6 px-4">
+                <DuePaymentsTable
+                    data={duesPayments ?? []}
+                />
+            </div>
+        </div>
     )
 }
+
