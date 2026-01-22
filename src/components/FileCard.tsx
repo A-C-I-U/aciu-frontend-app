bf0ea2f51a03ff83823bee0e61f1067f05a44453
Susu-spec <suleimansuwaibat@gmail.com>
2026-01-22 14:21:32 +0100

    refactor: change `ImageCard` to receive pdf files

--- src/components/FileCard.tsx
+++ src/components/FileCard.tsx
@@ -1,9 +1,9 @@
 import { DocumentDownload } from "iconsax-react";
 
-export const ImageCard = ({ imageUrl }: { imageUrl: string }) => {
+export const FileCard = ({ fileType, fileLabel, fileUrl }: { fileType: "img" | "pdf", fileLabel: string, fileUrl: string }) => {
 
     const handleDownload = () => {
-        window.open(imageUrl, "_blank", "noopener,noreferrer");
+        window.open(fileUrl, "_blank", "noopener,noreferrer");
     };
 
 
@@ -11,18 +11,18 @@ export const ImageCard = ({ imageUrl }: { imageUrl: string }) => {
         <div className="bg-card-200 border border-aciu-dashboard-background py-6 px-4 rounded-2xs w-full">
             <div className="flex gap-2 items-center">
                 <div className="border-[.7px] border-aciu-dashboard-background rounded-2xs bg-white size-12 flex items-center justify-center align-middle">
-                    <span className="text-xs leading-[120%] text-aciu-abriba">
-                        IMG
+                    <span className="text-xs leading-[120%] text-aciu-abriba capitalize">
+                        {fileType}
                     </span>
                 </div>
                 <div className="flex-1 w-full">
                     <p className="font-semibold leading-[120%] text-sm">
-                        Project Image
+                        {fileLabel}
                     </p>
                 </div>
                 <button
                     onClick={handleDownload}
-                    title="Download image"
+                    title="Download file"
                     type="button"
                 >
                     <DocumentDownload size={24} color="#00B686" />
