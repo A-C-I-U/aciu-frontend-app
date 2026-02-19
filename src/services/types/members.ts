3193dc0893070fbbd0c96e52961ca25822fc2d96
Susu-spec <suleimansuwaibat@gmail.com>
2026-02-19 19:21:48 +0100

    chore: update member types

---
+++ src/services/types/members.ts
@@ -0,0 +1,90 @@
+import type { TabItem } from "@/utils/types";
+import type { Event } from "./events";
+
+export interface MemberProfileOverview {
+  fullName: string;
+  occupation: string;
+  ageGrade: string;
+  isVerified: boolean;
+  phone: string;
+  email: string;
+  profilePhoto: string | null;
+}
+
+export interface GetMemberProfileResponse {
+  message: string;
+  profile: MemberProfileOverview;
+}
+
+
+interface ContributionMetric {
+  amount: number;
+  growth: string;
+}
+
+interface TimeUntilDue {
+  days: string | number;
+  hours: string | number;
+  minutes: string | number;
+  seconds: string | number;
+}
+
+interface NextMonthlyDue {
+  amount: number;
+  timeUntilDue: TimeUntilDue;
+}
+
+export interface MemberContributionsSummary {
+  monthlyContributions: ContributionMetric;
+  projectDonations: ContributionMetric;
+  nextMonthlyDue: NextMonthlyDue;
+}
+
+export interface GetMemberDuesSummary {
+    totalContributions: ContributionMetric;
+    outstandingBalance: {
+        amount: number
+    },
+    nextMonthlyDue: NextMonthlyDue
+}
+
+
+export interface GetUpcomingEventsResponse {
+  message: string;
+  events: Event[];
+}
+
+export interface DashboardBranchInfo {
+  branchLogo: string | null;
+  branchTitle: string;
+  meetingLocation: string;
+  registeredMembers: number;
+  isActive: boolean;
+}
+
+export interface GetMemberDashboardResponse {
+  dashboard: DashboardBranchInfo;
+}
+
+interface DonationMetric {
+  amount: number;
+  growth: string;
+}
+
+interface CountMetric {
+  count: number;
+  growth: string;
+}
+
+export interface GetMemberImpactSummary {
+  totalDonations: DonationMetric;
+  projectsSupported: CountMetric;
+  eventsSupported: CountMetric;
+}
+
+export interface BranchOverviewProps {
+    activeTab: TabItem,
+    setActiveTab: (activeTab: TabItem) => void,
+    overviewData?: GetMemberDashboardResponse,
+    isLoading: boolean;
+}
