9886c5506dd37c1660de997e5c010c85ffc97a35
Susu-spec <suleimansuwaibat@gmail.com>
2026-03-09 17:36:37 +0100

    fix: build error

--- src/pages/my-branch/index.tsx
+++ src/pages/my-branch/index.tsx
@@ -1,9 +1,8 @@
 import type { ExtendedTabItem } from "@/utils/types";
-import { useEffect, useState } from "react";
 import { useMediaQuery } from "@mui/material";
 import MyBranchMobileOverview from "./components/overview/Mobile";
 import MyBranchDesktopOverview from "./components/overview/Desktop";
-import { useLocation, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { myBranchTabs } from "./components/MyBranchTabs";
 
 export default function MyBranchPage() {
@@ -16,16 +15,6 @@ export default function MyBranchPage() {
     const handleTabChange = (tab: ExtendedTabItem) => {
     setSearchParams({ tab: tab.key });
     };
-
-    const [openEventsSuccessDialog, setEventsSuccessOpenDialog] = useState(false);
-    const location = useLocation();
-    const eventTitle = location.state?.eventTitle;
-
-    useEffect(() => {
-        if (eventTitle) {
-            setEventsSuccessOpenDialog(true);
-        }
-    }, [eventTitle]);
     
     return (
         <>
